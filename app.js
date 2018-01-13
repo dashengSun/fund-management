b10f307c6005af303d3093b0a733989510bf6b14
kaichao <kaichaosuna@gmail.com>
2018-01-13 16:40:46 +0800

    using promise.

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
 const express = require("express");
 const IOTA = require("iota.lib.js");
-const addr = require("./lib/addr")
+var mongoose = require('mongoose');
+const addr = require("./lib/addr");
 
 const app =express();
+
 const iota = new IOTA({
   'provider': 'http://mainnet.iotachina.win:14224'
 });
-const seed = process.env.SEED;
-
 console.log("iota version:", iota.version);
 
+const seed = process.env.SEED;
+
+mongoose.connect('mongodb://localhost/test');
+
 app.get("/", (req, res) => res.send("hello, weiOTA"));
+
 app.get("/address/generate", (req, res) => {
-  addr.generateAddr(iota, seed, res);
+  const addrResult = addr.generateAddr(iota, seed);
+  addrResult.then(address => {
+    return res.send({'address': address});
+  });
 });
 
 app.listen(process.env.PORT, () => console.log(`Example app listening on port ${process.env.PORT}.`));
